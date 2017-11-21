a0ce3b1b018e9fd27f7bc3eb12329b4a3d3f11ef
tbaraza <tonida.baraza@andela.com>
2017-11-21 19:42:28 +0300

    feat(models): Add user model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      classMethods: {
+        associate(models) {
+          // associations can be defined here
+        },
+      },
+    },
+  );
+  return User;
+};
