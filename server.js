8720b37182b1ea10276d1592668faa589e23f0f2
Tonida Baraza <tonida.baraza@andela.com>
2017-11-28 19:42:05 +0300

    Develop (#4)
    * Setup CircleCI (#1)
    
    * ch(CircleCI): Add CircleCI config file
    
    * ch(CircleCi): fix failing build
    
    * ch: Add CircleCI badge
    
    * Database models setup (#2)
    
    * feat(models): setup database and  models
    
    * feat(models): Add user model
    
    * Ft setup routes controllers (#3)
    
    * ch(models): add sequelizerc file
    
    * feat(routes&contollers): Set up routes and controllers
    
    * ch(dubugger): Set up NodeJS debugger on vscode
    
    * ch(PR): Add PR template file
    
    * Ft setup routes controllers (#5)
    
    * feat(tests): Set up backend tests
    
    * ch(circleCI): Add test command to circleCI config
    
    * feat(webpack): Set up webpack (#6)
    
    - Integrate webpack in the server
    - Enable hot reloading
    - Set up jsx and css loaders
    - Add antd

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 import express from 'express';
 import path from 'path';
+import configFile from './server/config/config';
+
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+const env = process.env.NODE_ENV || 'development';
+const config = configFile[env];
+const router = require(`./server/${config.apiVersion}/routes`);
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './src/index.html'));
-});
+if (env === 'production') {
+  app.use(express.static(path.join(__dirname, 'dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+  router(app);
+} else {
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: false,
+    hot: true,
+    stats: {
+      colors: true,
+    },
+  }));
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/index.html'));
+  });
+  router(app);
+}
 
 app.listen(port, (err) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.info(`listening on port${port}`);
+    console.info(`listening on port ${port}`);
   }
 });
